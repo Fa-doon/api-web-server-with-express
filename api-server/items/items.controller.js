c47d1a9939ce100c3070dadbba0b9d60667d8786
Fadoon T <fadoon.tar@gmail.com>
2023-08-26 14:45:09 +0100

    added authentication

--- api-server/items/items.controller.js
+++ api-server/items/items.controller.js
@@ -1,12 +1,7 @@
-const express = require("express");
-const bodyParser = require("body-parser");
 const fs = require("fs");
 
-const itemsRouter = express.Router();
-itemsRouter.use(bodyParser.json());
-
-// Create an item
-itemsRouter.post("/", (req, res) => {
+//create item
+const postItem = (req, res) => {
   const file = fs.readFileSync("./db/items.json");
   const items = JSON.parse(file);
   const itemAdded = req.body;
@@ -24,39 +19,35 @@ itemsRouter.post("/", (req, res) => {
     }
     res.status(200).json(createdItem);
   });
-});
+};
 
-// Get all items
-itemsRouter.get("/", (req, res) => {
+const getAllItems = (req, res) => {
   const file = fs.readFileSync("./db/items.json");
   const items = JSON.parse(file);
   res.status(200).send(items);
-});
+};
 
-// Get one item
-itemsRouter.get("/:id", (req, res) => {
+//get one item
+const getOneItem = (req, res) => {
   const file = fs.readFileSync("./db/items.json");
   const items = JSON.parse(file);
   const id = req.params.id;
-
   const itemFound = items.find((item) => item.id == parseInt(id));
-
   if (!itemFound) {
-    res.status(404).send("Item not found");
+    res.status(404).json({ message: `Item not found` });
     return;
   }
   res.status(200).json(itemFound);
-});
+};
 
-//Update an item
-itemsRouter.put("/:id", (req, res) => {
+// update an item
+const updateItem = (req, res) => {
   const file = fs.readFileSync("./db/items.json");
   const items = JSON.parse(file);
-  const itemUpdate = req.body;
 
+  const itemUpdate = req.body;
   const id = req.params.id;
   const idFound = items.findIndex((item) => item.id == parseInt(id));
-
   if (idFound == -1) {
     res.status(404).json({ error: "id not found" });
     return;
@@ -69,28 +60,34 @@ itemsRouter.put("/:id", (req, res) => {
     }
     res.json(items[idFound]);
   });
-});
+};
 
-//Deleting an item
-itemsRouter.delete("/:id", (req, res) => {
+// delete an item
+const deleteItem = (req, res) => {
   const file = fs.readFileSync("./db/items.json");
   const items = JSON.parse(file);
 
   const id = req.params.id;
   const idFound = items.findIndex((item) => item.id == parseInt(id));
-
   if (idFound == -1) {
     res.status(404).json({ error: "id not found" });
     return;
   } else {
     items.splice(idFound, 1);
   }
+
   fs.writeFile("./db/items.json", JSON.stringify(items), (err) => {
     if (err) {
       res.status(500).json({ error: "An error occured" });
     }
-    res.json(items);
+    res.json({ message: `Item successfully deleted` });
   });
-});
+};
 
-module.exports = itemsRouter;
+module.exports = {
+  postItem,
+  getAllItems,
+  getOneItem,
+  updateItem,
+  deleteItem,
+};
